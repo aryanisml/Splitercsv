53f62ee84c2906e94dcf7904411a788a74f09d09
swapnil.kamble02@gmail.com <swapnil.kamble02@gmail.com>
2016-05-23 11:51:22 +0530

    first csv commit

---
+++ SplitCSV/SplitCSV.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SplitCSV
+{
+    partial class SplitCSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplitCSV));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtNumberOfLineCount = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chkHeader = new System.Windows.Forms.CheckBox();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Line Break Count";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(128, 9);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(283, 20);
+            this.txtSource.TabIndex = 2;
+            // 
+            // txtNumberOfLineCount
+            // 
+            this.txtNumberOfLineCount.Location = new System.Drawing.Point(128, 38);
+            this.txtNumberOfLineCount.Name = "txtNumberOfLineCount";
+            this.txtNumberOfLineCount.Size = new System.Drawing.Size(109, 20);
+            this.txtNumberOfLineCount.TabIndex = 3;
+            this.txtNumberOfLineCount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumberOfLineCount_KeyPress);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.Location = new System.Drawing.Point(407, 7);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "W";
+            // 
+            // chkHeader
+            // 
+            this.chkHeader.AutoSize = true;
+            this.chkHeader.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkHeader.Location = new System.Drawing.Point(270, 41);
+            this.chkHeader.Name = "chkHeader";
+            this.chkHeader.Size = new System.Drawing.Size(158, 17);
+            this.chkHeader.TabIndex = 5;
+            this.chkHeader.Text = "Include Header In Each File";
+            this.chkHeader.UseVisualStyleBackColor = true;
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSplit.Location = new System.Drawing.Point(128, 64);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(75, 22);
+            this.btnSplit.TabIndex = 6;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(228, 64);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(254, 19);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // SplitCSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(494, 95);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.chkHeader);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtNumberOfLineCount);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SplitCSV";
+            this.Text = "Spliter";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtNumberOfLineCount;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox chkHeader;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ SplitCSV/SplitCSV.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace SplitCSV
+{
+   
+
+    public partial class SplitCSV : Form
+    {
+        public SplitCSV()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.csv_text;
+            progressBar1.Visible = false;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+           
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            txtSource.Text = openFileDialog1.FileName;
+            if (!txtSource.Text.Contains(".csv"))
+            {
+                MessageBox.Show("Please select csv file");
+                txtSource.Text = string.Empty;
+            }
+        }
+
+        private void txtNumberOfLineCount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+                e.Handled = true;
+
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtSource.Text))
+            {
+                MessageBox.Show("Please select csv file");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtNumberOfLineCount.Text) || Convert.ToInt32(txtNumberOfLineCount.Text)==0)
+            {
+                MessageBox.Show("Please sepcify the split count");
+                return;
+
+            }
+            progressBar1.Visible = true;
+            string sourceFileName = txtSource.Text;
+            string [] arrayFiles = txtSource.Text.Split('\\');
+            StringBuilder sb = new StringBuilder();
+            int linesPerFile =Convert.ToInt32(txtNumberOfLineCount.Text);
+            string fileName = string.Empty;
+            foreach (var item in arrayFiles)
+            {
+                if (!item.Contains(".csv"))
+                {
+                    sb.Append(item + "\\");
+
+                }
+                else {
+                    fileName = item;
+                
+                }
+
+
+            }
+            fileName = fileName.ToLower();
+            fileName = fileName.Replace(".csv", "");
+            string destinationFileName = sb + fileName +"-{0}.csv ";
+
+            using (var sourceFile = new StreamReader(sourceFileName))
+            {
+
+                var fileCounter = 0;
+                var destinationFile = new StreamWriter(
+                    string.Format(destinationFileName, fileCounter + 1));
+
+                try
+                {
+                    var lineCounter = 0;
+
+                    string line;
+                    string headerline=string.Empty;
+                    progressBar1.Minimum = fileCounter;
+                    progressBar1.Maximum = linesPerFile;
+                    while ((line = sourceFile.ReadLine()) != null)
+                    {
+                        if (lineCounter == 0)
+                        {
+                            headerline = line;
+                        }
+                        // Did we reach the maximum number of lines for the file?
+                        if (lineCounter >= linesPerFile)
+                        {
+                            
+                            // Yep... Time to close this one and 
+                            // switch to another file
+                            lineCounter = 0;
+                            fileCounter++;
+
+                            destinationFile.Dispose();
+                            destinationFile = new StreamWriter(
+                                string.Format(destinationFileName, fileCounter + 1));
+                            if (!string.IsNullOrEmpty(headerline) && chkHeader.Checked)
+                            {
+                                destinationFile.WriteLine(headerline);
+                            }
+                            
+                        }
+                        progressBar1.Value = fileCounter;    
+                        destinationFile.WriteLine(line);
+                        lineCounter++;
+                    }
+
+                    progressBar1.Value = progressBar1.Maximum;
+                    MessageBox.Show("Split Completed");
+                    progressBar1.Visible = false;
+                    txtNumberOfLineCount.Text = string.Empty;
+                    txtSource.Text = string.Empty;
+                    chkHeader.Checked = false;
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    destinationFile.Dispose();
+                }
+            }
+
+        }
+    }
+}
